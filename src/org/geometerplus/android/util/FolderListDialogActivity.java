ab0ffe723d5d00afaf5300aab7d71714b24ffc8d
Ahmed Saidi <justroftest@gmail.com>
2014-05-07 15:20:32 +0100

    Fix last merge

--- src/org/geometerplus/android/util/FolderListDialogActivity.java
+++ src/org/geometerplus/android/util/FolderListDialogActivity.java
@@ -29,7 +29,7 @@ import android.view.*;
 import android.widget.*;
 
 import org.geometerplus.zlibrary.core.resources.ZLResource;
-import org.geometerplus.zlibrary.ui.android.R;
+import net.sourceforge.arabicReader.R;
 
 public class FolderListDialogActivity extends ListActivity {
 	interface Key {
